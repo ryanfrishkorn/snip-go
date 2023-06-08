685999479f337450415b600488d7c37c0918402b
Ryan Frishkorn <ryan@bluerift.io>
2023-06-08 15:19:39 -0700

    Add timestamp generation

--- snip.go
+++ snip.go
@@ -2,21 +2,21 @@ package snip
 
 import (
 	"github.com/google/uuid"
+	"time"
 )
 
+// Snip represents a snippet of data with additional metadata
 type Snip struct {
-	UUID uuid.UUID
-	Data []byte
+	Data      []byte
+	Timestamp time.Time
+	UUID      uuid.UUID
 }
 
 // New returns a new snippet and generates a new UUID for it
 func New() (Snip, error) {
 	return Snip{
-		UUID: CreateUUID(),
-		Data: []byte{},
+		Data:      []byte{},
+		Timestamp: time.Now(),
+		UUID:      uuid.New(),
 	}, nil
-}
-
-func CreateUUID() uuid.UUID {
-	return uuid.New()
 }
